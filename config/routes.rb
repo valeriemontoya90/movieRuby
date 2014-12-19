7526d9f6b219488247ad3f3a6e668a384d0a477f
Valerie Montoya <valeriemontoya90@gmail.com>
2014-12-19 14:47:26 +0100

    change scaffold user into devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :user_movies
 
   resources :movies
 
-  resources :users
+  get '/' => 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
